15810ccaa25f93175d7c87f90f3e8913045aaed9
Arjunan K <arjunank680620@gmail.com>
2023-07-22 23:57:37 +0530

    Add users crud controller and router

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,7 +1,43 @@
-import { json } from "body-parser";
-import { createUser, getUserByEmail } from "db/users";
+import { createUser, getUserByEmail } from "../db/users";
 import express from "express";
-import { authentication, random } from "helpers";
+import { authentication, random } from "../helpers";
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+    if (user.authentication.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+    await user.save();
+    res.cookie("ARJUNAN_K_API_AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
@@ -19,7 +55,7 @@ export const register = async (req: express.Request, res: express.Response) => {
     const salt = random();
     const user = await createUser({
       email,
-      password,
+      username,
       authentication: {
         password: authentication(salt, password),
         salt: salt,
--- src/db/users.ts
+++ src/db/users.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
   email: { type: String, required: true },
+  username: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
     salt: { type: String, select: false },
